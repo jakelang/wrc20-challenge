48730d89aac10a591a47f3e38d2cfb8a379ffc04
Jake Lang <phazon@protonmail.com>
2018-06-27 01:32:17 -0400

    cleanup

--- main.c
+++ main.c
@@ -1,43 +1,24 @@
 #include <ethereum.h>
 
-void 
-main(void)
-{
-	if (eei_callDataSize < 4)
-		eei_revert(0, 0);
-	
-	uint32_t selector;
-	eei_callDataCopy(&selector, 0, 4);
-
-	switch (selector) {
-	case 0x9993021a:
-		do_balance();
-	case 0x5d359fbd:
-		do_transfer();
-	default:
-		eei_revert(0, 0);
-	}
-}
-
 void
-do_balance()
+do_balance(void)
 {
-	if (eei_callDataSize != 24)
+	if (eei_getCallDataSize() != 24)
 		eei_revert(0, 0);
 	
-	struct evm_address address;
+	struct evm_uint256 address;
 	struct evm_uint256 balance;
 
-	eei_callDataCopy(&address, 4, 20);
+	eei_callDataCopy(address.bytes, 4, 20);
 	eei_storageLoad(&address, &balance);
 
-	eei_return(&balance, 8);
+	eei_return(balance.bytes, 8);
 }
 
 void 
-do_transfer()
+do_transfer(void)
 {
-	if (eei_callDataSize != 32)
+	if (eei_getCallDataSize() != 32)
 		eei_revert(0, 0);
 
 	struct evm_uint256 sender;
@@ -51,15 +32,15 @@ do_transfer()
 	uint64_t *recipient_balance_raw;
 	uint64_t *value_raw;
 
-	eei_getTxOrigin(&sender);
-	eei_callDataCopy(&recipient, 4, 20);
-	eei_callDataCopy(&value, 24, 8);
+	eei_getTxOrigin((struct evm_address *)&sender);
+	eei_callDataCopy(recipient.bytes, 4, 20);
+	eei_callDataCopy(value.bytes, 24, 8);
 	eei_storageLoad(&sender, &sender_balance);
 	eei_storageLoad(&recipient, &recipient_balance);
 	
-	sender_balance_raw = &sender_balance;
-	recipient_balance_raw = &recipient_balance;
-	value_raw = &value;
+	sender_balance_raw = (uint64_t *)&sender_balance;
+	recipient_balance_raw = (uint64_t *)&recipient_balance;
+	value_raw = (uint64_t *)&value;
 
 	if (*sender_balance_raw < *value_raw)
 		eei_revert(0, 0);
@@ -69,4 +50,23 @@ do_transfer()
 
 	eei_storageStore(&sender, &sender_balance);
 	eei_storageStore(&recipient, &recipient_balance);
+}
+
+void 
+main(void)
+{
+	if (eei_getCallDataSize() < 4)
+		eei_revert(0, 0);
+	
+	uint32_t selector;
+	eei_callDataCopy((uint8_t *)&selector, 0, 4);
+
+	switch (selector) {
+	case 0x9993021a:
+		do_balance();
+	case 0x5d359fbd:
+		do_transfer();
+	default:
+		eei_revert(0, 0);
+	}
 }
