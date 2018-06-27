b388e0a01da1fb57158eeffe5c0085ffd53fe5ff
Jake Lang <phazon@protonmail.com>
2018-06-27 01:08:10 -0400

    implement wrc20 challenge

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 void 
 main(void)
 {
-	if (ethereum_callDataSize < 4)
-		ethereum_revert(0, 0);
+	if (eei_callDataSize < 4)
+		eei_revert(0, 0);
 	
 	uint32_t selector;
-	ethereum_callDataCopy(&selector, 0, 4);
+	eei_callDataCopy(&selector, 0, 4);
 
 	switch (selector) {
 	case 0x9993021a:
@@ -15,28 +15,58 @@ main(void)
 	case 0x5d359fbd:
 		do_transfer();
 	default:
-		ethereum_revert(0, 0);
+		eei_revert(0, 0);
 	}
 }
 
-void 
+void
 do_balance()
 {
-	if (ethereum_callDataSize != 24)
-		ethereum_revert(0, 0);
+	if (eei_callDataSize != 24)
+		eei_revert(0, 0);
+	
+	struct evm_address address;
+	struct evm_uint256 balance;
+
+	eei_callDataCopy(&address, 4, 20);
+	eei_storageLoad(&address, &balance);
+
+	eei_return(&balance, 8);
+}
+
+void 
+do_transfer()
+{
+	if (eei_callDataSize != 32)
+		eei_revert(0, 0);
 
 	struct evm_uint256 sender;
 	struct evm_uint256 recipient;
-	struct evm_value value;
-	struct evm_value sender_balance;
-	struct evm_value recipient_balance;
+	struct evm_uint256 value;
+	struct evm_uint256 sender_balance;
+	struct evm_uint256 recipient_balance;
 
-	ethereum_getTxOrigin(&sender);
-	ethereum_callDataCopy(&recipient, 4, 20);
-	ethereum_callDataCopy(&value, 24, 8);
-	ethereum_storageLoad(&sender, &sender_balance);
-	ethereum_storageLoad(&recipient, &recipient_balance);
+	/* Use these pointers to treat balance values as normal integers */
+	uint64_t *sender_balance_raw;
+	uint64_t *recipient_balance_raw;
+	uint64_t *value_raw;
 
-	// if sender balance < val
-	// revert (0,0)
+	eei_getTxOrigin(&sender);
+	eei_callDataCopy(&recipient, 4, 20);
+	eei_callDataCopy(&value, 24, 8);
+	eei_storageLoad(&sender, &sender_balance);
+	eei_storageLoad(&recipient, &recipient_balance);
+	
+	sender_balance_raw = &sender_balance;
+	recipient_balance_raw = &recipient_balance;
+	value_raw = &value;
+
+	if (*sender_balance_raw < *value_raw)
+		eei_revert(0, 0);
+
+	*sender_balance_raw -= *value_raw;
+	*recipient_balance_raw += *value_raw;
+
+	eei_storageStore(&sender, &sender_balance);
+	eei_storageStore(&recipient, &recipient_balance);
 }
